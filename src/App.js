64489d5e84e1359fb8a5e797bf6037799ef85a7b
Anton Drevetskyi <drevetskyianton@gmail.com>
2022-02-04 15:51:34 -0700

    "Update parent state from child (callback)"

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import DisplayArray from "./Component/DisplayArray/DisplayArray";
 import ShowHide from "./Component/ShowHide/ShowHide";
 import DataBind from "./Component/DataBind/DataBind";
 import DisableButton from "./Component/DisableButton/DisableButton";
+import ParentState from "./Component/ParentState/ParentState";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <ShowHide />
       <DataBind />
       <DisableButton />
+      <ParentState />
     </div>
   );
 }
---
+++ src/Component/ParentState/ParentState.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState } from "react";
+
+function Child({ setValue }) {
+  return (
+    <>
+      <div>Child</div>
+      <button onClick={() => setValue("Parent has been updated!")}>
+        Change parent state
+      </button>
+    </>
+  );
+}
+
+function ParentState() {
+  const [value, setValue] = useState("Update me from child");
+  return (
+    <>
+      <div className="showhide">
+        <h3>Update Parent State (callback)</h3>
+        <div>Parent</div>
+        <div>{value}</div>
+        <h4>Press to update parent state</h4>
+        <Child setValue={setValue} />
+      </div>
+    </>
+  );
+}
+
+export default ParentState;
